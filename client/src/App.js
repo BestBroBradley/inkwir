5127118bcb487fd280c6d687e24b6e3a66e11067
Katie88 <kathrynhoth@gmail.com>
2020-03-02 14:51:29 -0700

    adding page files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,50 @@
+import React, { Component } from 'react';
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Accounts from "./components/pages/Accounts";
+import Create from "./components/pages/Create";
+import Homepage from "./components/pages/Homepage";
+import Loggedin from "./components/pages/Loggedin";
+import Results from "./components/pages/Results";
+import Survey from "./components/pages/Survey";
+import Update from "./components/pages/Update";
+import Background from "./components/background_image.jpg"
+
+var sectionStyle = {
+    width: "100%",
+    height: "400px",
+    backgroundImage: "url(" + {Background} + ")"
+};
+
+class Section extends Component {
+    render(){
+        return (
+            <section style={sectionStyle}>
+            </section>
+        );
+    }
+}
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    function App() {
+        return (
+          <Router>
+            <div>
+              <NavTabs />
+              <Route exact path="/" component={Homepage} />
+              <Route exact path="/accounts" component={Accounts} />
+              <Route exact path="/create" component={Create} />
+              <Route exact path="/loggedin" component={Loggedin} />
+              <Route exact path="/results" component={Results} />
+              <Route exact path="/survey" component={Survey} />
+              <Route exact path="/update" component={Update} />
+            </div>
+          </Router>
+        );
+      };
+    }
+      
 export default App;
---
+++ client/src/components/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Homepage() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+     This is the homepage
+      </p>
+    </div>
+  );
+}
+
+export default Homepage;

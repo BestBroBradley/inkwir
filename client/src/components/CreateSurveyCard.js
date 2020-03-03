4d9b6976814316a2ea945572e1aa6e596a491e6f
cake <larosteguy@gmail.com>
2020-03-02 19:51:33 -0700

    adding component files

---
+++ client/src/components/CreateSurveyCard.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Input, Card, Button } from 'semantic-ui-react'
+
+const CreateSurvey= () => (
+  <div>
+    <Card color='pink'> 
+      <h3>Create a New Survey!</h3>
+      <Input fluid placeholder='Survey Topic:' />
+      <br/>
+    <Input fluid placeholder='Enter a QUESTION here ...' />
+      <br/>
+      <br/>
+    <Input fluid placeholder='Enter an ANSWER here ...' />
+      <br/>
+    <Input fluid placeholder='Enter an ANSWER here ...' />
+      <br/>
+    <Input fluid placeholder='Enter an ANSWER here ...' />
+      <br/>
+    <Input fluid placeholder='Enter an ANSWER here ...' />
+      <br/>
+    </Card> 
+    <Button color='pink'>Add another question!</Button>
+    <Button color='pink'>Submit your survey!</Button>
+  </div>
+)
+
+export default CreateSurvey;

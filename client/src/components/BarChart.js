400509f2122a799b918578f32f689d2e858e1e3f
Allison Bradley <allison.bradley@outlook.com>
2020-03-09 20:28:25 -0600

    Worked on individual survey functionality

--- client/src/components/BarChart.js
+++ client/src/components/BarChart.js
@@ -20,7 +20,34 @@ const BarChart = () => {
     <div>
       {results.map(result =>
         <div>
-          <ChartInstance results={result} />
+          <h5 id="answers">Title: {result.title}</h5>
+          <h5 id="fontPatrol">Category: {result.category}</h5>
+          { result.questions[0].q1 ? 
+          <ChartInstance 
+            q={result.questions[0].q1}
+            a={result.questions[0].a1}
+            r={result.results.a1}/> : null }
+          { result.questions[0].q2 ? 
+          <><br/><ChartInstance 
+            q={result.questions[0].q2}
+            a={result.questions[0].a2}
+            r={result.results.a2}/></> : null }
+          { result.questions[0].q3 ? 
+          <><br/><ChartInstance 
+            q={result.questions[0].q3}
+            a={result.questions[0].a3}
+            r={result.results.a3}/></> : null }
+          { result.questions[0].q4 ? 
+          <><br/><ChartInstance 
+            q={result.questions[0].q4}
+            a={result.questions[0].a4}
+            r={result.results.a4}/></> : null }
+          { result.questions[0].q5 ? 
+          <><br/><ChartInstance 
+            q={result.questions[0].q5}
+            a={result.questions[0].a5}
+            r={result.results.a5}/></> : null }
+          {/* <ChartInstance results={result} /> */}
         </div>)}
     </div>
   )
